369eec64b8a3b4f7b2ad5ccf79242d052db1cb2e
bhargavasai227 <112853876+bhargavasai227@users.noreply.github.com>
2024-03-09 16:06:46 +0530

    added resume

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,6 +1,7 @@
 import { FaGithub } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
+import { MdFileDownload } from "react-icons/md";
 import Link from "next/link";
 function Contact() {
   return (
@@ -8,7 +9,7 @@ function Contact() {
         <div className="flex gap-1 font-thin items-center"><FaLinkedin/><Link target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/bhargavasaidasari/">Linkedin</Link></div>
         <div className="flex gap-1 font-thin items-center"><FaGithub/>  <Link target="_blank" rel="noopener noreferrer" href="https://github.com/bhargavasai227/">GitHub</Link></div>
         <div className="flex gap-1 font-thin items-center"><BiLogoGmail/>  <a target="_blank" rel="noopener noreferrer" href="mailto:bhargavdasari227@gmail.com">Gmail</a></div>
-
+        <div className="flex gap-1 font-thin items-center"><MdFileDownload/><Link href="https://www.dropbox.com/scl/fi/mb9uakdba7otu0ya6eaho/BhargavDasariResume.pdf?rlkey=hdzo19lvlnx1uuxuoc4b0t10d&dl=0">Resume</Link></div>
     </div>
   )
 }
---
+++ app/components/Pdf.tsx
@@ -0,0 +1,6 @@
+
+export default function Pdf(){
+return(
+    <iframe src="../../public/db.pdf" width="100%" height="500px" />
+)
+}
